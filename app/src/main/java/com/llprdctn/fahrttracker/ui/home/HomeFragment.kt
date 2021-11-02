746611834d9f715bcd76690a882392dacd53fb2e
LuX1nAt0r <66088547+LuX1nAt0r@users.noreply.github.com>
2021-11-02 21:33:15 +0100

    Added HomeFragment with navigation

--- app/src/main/java/com/llprdctn/fahrttracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/llprdctn/fahrttracker/ui/home/HomeFragment.kt
@@ -1,8 +1,29 @@
 package com.llprdctn.fahrttracker.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.llprdctn.fahrttracker.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cvAddDrive.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToFragmentAddEditDrive()
+            findNavController().navigate(action)
+        }
+
+        cvAddMitfahrer.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToFragmentAddEditMitfahrer()
+            findNavController().navigate(action)
+        }
+
+    }
+
 }
