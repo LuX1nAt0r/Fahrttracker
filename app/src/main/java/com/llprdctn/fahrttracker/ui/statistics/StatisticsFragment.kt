5f7bd57c8b1309db71b9c1778ab26d7402a5c71b
LuX1nAt0r PC <l.landsteiner@t-online.de>
2021-11-22 16:38:28 +0100

    Added charts dependencies

--- app/src/main/java/com/llprdctn/fahrttracker/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/llprdctn/fahrttracker/ui/statistics/StatisticsFragment.kt
@@ -1,9 +1,24 @@
 package com.llprdctn.fahrttracker.ui.statistics
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.llprdctn.fahrttracker.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /*val barChart = getView()?.findViewById<BarChart>(R.id.barChart)
+        BarEntry()
+        BarDataSet(List<BarEntry>)*/
+
+    }
 }
