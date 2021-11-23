b1872a9a4658d35cdd8eaf13022577fc05520936
LuX1nAt0r PC <l.landsteiner@t-online.de>
2021-11-23 22:53:44 +0100

    Modified the statistics Fragment

--- app/src/main/java/com/llprdctn/fahrttracker/other/Constants.kt
+++ app/src/main/java/com/llprdctn/fahrttracker/other/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
     const val DATABASE_NAME = "fahrtTracker_db"
 
     const val SHARED_PREFERENCES = "sharedPreferencesFahrttracker"
+
+    const val FUEL_PRICE = 1.5
+    const val FUEL_USAGE_PER_KM = 0.09f
+    const val DISTANCE = 29
 }
--- app/src/main/java/com/llprdctn/fahrttracker/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/llprdctn/fahrttracker/ui/statistics/StatisticsFragment.kt
@@ -4,19 +4,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.llprdctn.fahrttracker.R
+import com.llprdctn.fahrttracker.data.entities.Drive
+import com.llprdctn.fahrttracker.other.Constants.DISTANCE
+import com.llprdctn.fahrttracker.other.Constants.FUEL_PRICE
+import com.llprdctn.fahrttracker.other.Constants.FUEL_USAGE_PER_KM
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_statistics.*
 
 @AndroidEntryPoint
 class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
 
     private val viewModel: StatisticsViewModel by viewModels()
+    private var allDrives: MutableList<Drive> = mutableListOf()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        subscribeToObservers()
 
+    }
 
+    private fun subscribeToObservers() {
+        viewModel.allDrives.observe(viewLifecycleOwner, Observer {
+            allDrives = it.toMutableList()
+            tvDrivesCount.text = it.size.toString()
+            tvtotalDistanceCount.text = (it.size * DISTANCE).toString() + "km"
+            val price = ((it.size * DISTANCE).toFloat() * FUEL_USAGE_PER_KM * FUEL_PRICE)
+
+            tvMoneyCount.text = "%.2f".format(price)+ " €"
+        })
     }
 }
